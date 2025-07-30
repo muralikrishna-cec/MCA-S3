40c73874df6e0d1195602e13ff349b9291d74d66
MuraliKrishna <mailstomuralikrishna@gmail.com>
2025-07-30 23:10:14 +0530

    prgm-1 added

---
+++ DS|ML/CYCLE-1/1.py
@@ -0,0 +1,11 @@
+print("Demo of basic datatypes:Numbers")
+x=3
+y=2.5
+print("x=",x)
+print("y=",y)
+print("Datatype of variable x:",type(x))
+print("Datatype of variable y:",type(y))
+print("Addition:",x+y)
+print("Subtraction:",x-y)
+print("Multiplication:",x*2)
+print("Exponentation:",x**2)
