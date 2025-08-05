fc5a3a29e0b066773403fb672e9bec17cf733db4
Murali Krishna <chn24mc018@ceconline.edu>
2025-08-05 15:00:27 +0530

    Fb page prgm completed

---
+++ MAD/CYCLE-2/FaceBookpage/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.fcaebookpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView facebookView=findViewById(R.id.fbView);
+        ImageView likeImgView=findViewById(R.id.likeView);
+        ImageView commentImgView=findViewById(R.id.cmmntView);
+        ImageView shareImgView=findViewById(R.id.shareView);
+
+        likeImgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("You Clicked Like Button");
+            }
+        });
+
+        commentImgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("You Clicked Comment Button");
+            }
+        });
+        shareImgView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("You Clicked Share Button");
+            }
+        });
+
+
+    }
+
+    private void showToast(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
