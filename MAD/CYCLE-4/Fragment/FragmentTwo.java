43fc3a03f1bd50c698c8bfd2290b362334d22122
MuraliKrishna <mailstomuralikrishna@gmail.com>
2025-09-25 22:43:45 +0530

    Cycle 4 prgm 1 added

---
+++ MAD/CYCLE-4/Fragment/FragmentTwo.java
@@ -0,0 +1,21 @@
+package com.example.fragments39;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentTwo extends Fragment {
+
+    @Nullable
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+}
